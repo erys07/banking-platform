ace5d137e56fe73cf2b4d619cffdb12bb685b51f
Erivaldo Cavalcante <cavalcanteerivaldo0@gmail.com>
2025-11-23 03:35:18 -0300

    initial commit

---
+++ src/main/kotlin/com/project/banking_platform/BankingPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.project.banking_platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/banking_platform/BankingPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.banking_platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankingPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
